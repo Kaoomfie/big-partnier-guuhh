774d72a8c29d73edfafd5587be2ac3a656b2086d
Ola Nikolai Skjeret <fyr@hotmail.no>
2024-07-09 22:48:35 +0200

    wtf am I doing bruh

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let data;
 const bigFckuckingArray = [];
 
 // Fetch JSON data, and run initialRender function once finished
-fetch("big-partnier-guuhh/pardner.json")
+fetch("https://kaoomfie.github.io/big-partnier-guuhh/pardner.json")
     .then((res) => res.json())
     .then((json) => data = json)
     .then(() => initialRender(data));
