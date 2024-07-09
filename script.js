3951bf1756d595b03fe225d3f77a628be6db0b3c
Ola Nikolai Skjeret <fyr@hotmail.no>
2024-07-10 01:13:47 +0200

    added funny collapsibles, populated item sets

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Collapsiple content button logic
+const coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+          } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+          }
+    });
+}
+
+
 // Define some variables
 let data;
 const bigFckuckingArray = [];
@@ -12,10 +28,59 @@ function initialRender(data) {
     // Personal yoinkage
     console.log(data);
 
-    // Max stats
-    // Sets slot and cost values from JSON data
-    document.querySelector(".slots").innerHTML = data["Maxed Partner"]["Skill Slots"];
-    document.querySelector(".cost").innerHTML = data["Maxed Partner"]["Max Cost"];
+
+    // Skills
+
+
+
+    // Auto skills
+
+
+
+    // Item sets
+    const itemSetKeys = [Object.keys(data["Items"]["Normal"]), Object.keys(data["Items"]["Urgent"])]
+    const normalList = document.querySelector(".normal-sets");
+    const urgentList = document.querySelector(".urgent-sets");
+    const itemSetTemplate = normalList.children[0];
+    for (let i = 0; i < itemSetKeys.length; i++) {
+        // Normal
+        if (i == 0) {
+            itemSetKeys[i].forEach((element) => {
+                if (itemSetKeys[i].indexOf(element) != 0) {
+                    const newItemSet = itemSetTemplate.cloneNode(true);
+                    newItemSet.children[0].innerHTML = element;
+                    newItemSet.children[1].innerHTML = data["Items"]["Normal"][element]["GCP Cost"];
+                    newItemSet.children[2].innerHTML = "";
+                    data["Items"]["Normal"][element]["Items"].forEach((item) => {
+                        const newItem = document.createElement("li");
+                        newItem.innerHTML = item;
+                        newItemSet.children[2].append(newItem);
+                    })
+                    normalList.append(newItemSet);
+                } else {
+                    itemSetTemplate.children[0].innerHTML = element;
+                    itemSetTemplate.children[1].innerHTML = data["Items"]["Normal"][element]["GCP Cost"];
+                    data["Items"]["Normal"][element]["Items"].forEach((item) => {
+                        const newItem = document.createElement("li");
+                        newItem.innerHTML = item;
+                        itemSetTemplate.children[2].append(newItem);
+                    })
+                }
+            })
+        // Urgent
+        } else if (i == 1) {
+            itemSetKeys[i].forEach((element) => {
+                urgentList.children[0].children[0].innerHTML = element;
+                urgentList.children[0].children[1].innerHTML = data["Items"]["Urgent"][element]["GCP Cost"];
+                data["Items"]["Urgent"][element]["Items"].forEach((item) => {
+                    const newItem = document.createElement("li");
+                    newItem.innerHTML = item;
+                    urgentList.children[0].children[2].append(newItem);
+                })
+            })
+        }
+    }
+
 
     // Weapon unlocks
     // Create an array of the "Weapons" keys to iterate through
@@ -26,7 +91,8 @@ function initialRender(data) {
     // Iterate through the keys to add the needed nodes, and update their values
     weaponUnlockKeys.forEach((element) => {
         // For all elements except the 2nd, create a cloned node
-        if (element != "Second") {
+        // if (element != "Second") {
+        if (weaponUnlockKeys.indexOf(element) != 0) {
             const newUnlock = unlockTemplate.cloneNode(true);
             // Update the cloned node's values
             newUnlock.children[0].innerHTML = element.toLocaleLowerCase();
@@ -44,6 +110,11 @@ function initialRender(data) {
         bigFckuckingArray.push(data["Weapons"][element]["GCP Cost"]);
     });
 
+
+    // Max stats
+    // Sets slot and cost values from JSON data
+    document.querySelector(".slots").innerHTML = data["Maxed Partner"]["Skill Slots"];
+    document.querySelector(".cost").innerHTML = data["Maxed Partner"]["Max Cost"];
 
 
     // Sum the whole ass huge fucking costs and print the result
